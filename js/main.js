1c34dc1dcde07d4b8f2f51107204dd4f08da0523
Dan Benson <dbenson3091@gmail.com>
2021-09-03 12:53:38 -0500

    added star features for selecting favorite stories

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ const $navLogOut = $("#nav-logout");
 const $navSubmit = $("#nav-new-story");
 const $newStoryForm = $("#new-story-form");
 
-
 /** To make it easier for individual components to show just themselves, this
  * is a useful function that hides pretty much everything on the page. After
  * calling this, individual components can re-show just what they want.
--- js/stories.js
+++ js/stories.js
@@ -23,9 +23,15 @@ async function getAndShowStoriesOnStart() {
 function generateStoryMarkup(story) {
   console.debug("generateStoryMarkup", story);
 
+  //1. Line 30: add two font awesome star images
+  //2. Create a function that toggles favorites when stars are clicked on
+  //3. Add an event listener to the stars when clicked on
+  //4. Update css class of hidden between both font awesome stars
   const hostName = story.getHostName();
   return $(`
       <li id="${story.storyId}">
+        <i class="far fa-star"></i>
+        <i class="fas fa-star hidden"></i>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -75,3 +81,33 @@ function putStoriesOnPage() {
 
   $allStoriesList.show();
 }
+
+function toggleStar(e) {
+  console.log("target success", e.target);
+  const $currentStar = $(e.target).eq(0);
+  const $solidStar = $(".fas").eq(0);
+  $currentStar.toggleClass("hidden");
+  $solidStar.toggleClass("hidden");
+
+  // $currentStar.toggleClass("hidden");
+  // $solidStar.show();
+  // console.log($currentStar.closest("i"), "closest icon");
+  // console.log("current star", $currentStar.closest(".far"));
+  // console.log("solid star", $currentStar.closest(".fas"));
+
+  // if ($currentStar.hasClass("far")) {
+  //   $currentStar.hide();
+  //   $currentStar.closest(".fas").show();
+  //   // $currentStar.show();
+  // }
+
+  // console.log($starFind, "found star");
+  // if (e.target.classList.value === "far fa-star") {
+  //   // e.target.closest(".fas").show();
+  //   e.target.hide();
+  // }
+
+  // console.log(e.target.closest(".hidden"));
+}
+
+$allStoriesList.on("click", ".fa-star", toggleStar);
